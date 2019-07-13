8737b38e10cf356eb13f6c41a166a87a5a1c6c5b
Ubuntu <ubuntu@ip-172-31-42-224.us-west-2.compute.internal>
2019-07-13 03:34:52 +0000

    fix the bug

--- test2.py
+++ test2.py
@@ -1,45 +1,53 @@
 import sys
 
-if __name__ == '__main__':
-    file_name = sys.argv[1]
+def solve(fine_name):
     fd = open(file_name,'r')
     productDict = {}
     for line in fd.readlines():
         lineList = line.strip().replace('\"',"").replace('{','').replace('}','').split(',')
-        productId = lineList[1].split(':')[1]
-        quantity = lineList[2].split(':')[1]
+        user = lineList[0].split(':')[1].strip()
+        productId = lineList[1].split(':')[1].strip()
+        quantity = lineList[2].split(':')[1].strip()
         if not productDict.has_key(productId):
-            productDict[productId] = [1, int(quantity)]
+            productDict[productId] = [1, int(quantity), [user]]
         else:
-            productDict[productId][0] += 1
             productDict[productId][1] += int(quantity)
+            if not user in productDict[productId][2]:
+                productDict[productId][0] += 1
+                productDict[productId][2].append(user)
         
-        maxPurchasedIdList = []
-        maxPurchasedValue = 0 
-        maxSoldQuantityIdList = []
-        maxSoldQuantityValue = 0 
+    maxPurchaserIdList = []
+    maxPurchaserValue = 0 
+    maxSoldQuantityIdList = []
+    maxSoldQuantityValue = 0 
 
-        for key, value in productDict.items():
-            if value[0] > maxPurchasedValue:
-                maxPurchasedIdList = [key.strip()]
-                maxPurchasedValue = value[0]
-            elif value[0] == maxPurchasedValue:
-                maxPurchasedIdList.append(value[0])
+    for key, value in productDict.items():
+        if value[0] > maxPurchaserValue:
+            maxPurchaserIdList = [key]
+            maxPurchaserValue = value[0]
+        elif value[0] == maxPurchaserValue:
+            if not key in maxPurchaserIdList:
+                maxPurchaserIdList.append(key)
 
-            if value[1] > maxSoldQuantityValue:
-                maxSoldQuantityIdList = [key.strip()]
-                maxSoldQuantityValue = value[1]
-            elif value[1] == maxSoldQuantityValue:
-                maxSoldQuantityIdList.append(value[1])
+        if value[1] > maxSoldQuantityValue:
+            maxSoldQuantityIdList = [key]
+            maxSoldQuantityValue = value[1]
+        elif value[1] == maxSoldQuantityValue:
+            if not key in maxSoldQuntityIdList:
+                maxSoldQuantityIdList.append(key)
     
-    PurcharsedPrefix = 'Most popular product(s) based on the number of purchasers: [ '
-    PurcharsedContent = []
-    for id in maxPurchasedIdList:
-        PurcharsedContent.append('\"' + id + '\"')
-    print PurcharsedPrefix + ','.join(PurcharsedContent) + ' ]'
+    PurcharserPrefix = 'Most popular product(s) based on the number of purchasers: [ '
+    PurcharserContent = []
+    for productId in maxPurchaserIdList:
+        PurcharserContent.append('\"' + productId + '\"')
+    print PurcharserPrefix + ','.join(PurcharserContent) + ' ]'
             
     SoldQuantityPrefix = 'Most popular product(s) based on the quantity of goods sold: [ '
     SoldQuantityContent =[] 
-    for id in maxSoldQuantityIdList:
-        SoldQuantityContent.append('\"' + id + '\"')
+    for productId in maxSoldQuantityIdList:
+        SoldQuantityContent.append('\"' + productId + '\"')
     print SoldQuantityPrefix + ','.join(SoldQuantityContent) + ' ]'
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    solve(file_name)
